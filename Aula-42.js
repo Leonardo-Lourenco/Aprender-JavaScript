4441dc0922ecace31a66f3b9e6eb9d921817414a
Leonardo Lourenco <lnddru@gmail.com>
2020-12-16 20:21:55 -0300

    Add files via upload

---
+++ Aula-42.js
@@ -0,0 +1,94 @@
+
+
+// Objetos do Tipo Number 
+
+/*
+const a = 45;
+
+const b = new Number(45);
+
+console.log(a);
+console.log(b); // 45
+
+
+console.log(typeof a);
+console.log(typeof b);
+
+
+
+
+
+
+// Métodos de número em JavaScript 
+
+
+
+Método	
+													Descrição
+
+isNaN ()										determina se o valor passado é NaN
+isFinite ()										determina se o valor passado é um número finito
+isInteger ()									determina se o valor passado é um inteiro
+isSafeInteger ()								determina se o valor passado é um número inteiro seguro
+parseFloat(string)								converte a string numérica flutuante em um número de ponto flutuante
+parseInt(string, [radix])						converte a string numérica em inteiro
+toExponential(fractionDigits)					retorna um valor de string para um número em notação exponencial
+toFixed (dígitos)								retorna um valor de string para um número em notação de ponto fixo
+toPrecision ()									retorna um valor de string para um número com uma precisão especificada
+toString ([radix])								retorna um valor de string em uma raiz especificada (base)
+valor de()										retorna o valor dos números
+toLocaleString()								retorna uma string com uma representação sensível ao idioma de um número
+
+
+
+
+
+// Alguns exemplos
+
+const a = 12;
+console.log(Number.isInteger(a)); // true
+
+const b = NaN; 
+console.log(Number.isNaN(b)); //true
+
+const c = 5.1234;
+console.log(c.toFixed(2)); // 5.12
+
+
+
+
+
+
+
+// Propriedades de Número JS
+
+
+// Aqui está uma lista de propriedades Number em JavaScript.
+
+
+Propriedade													Descrição
+
+EPSILON										retorna o menor intervalo entre dois números representáveis
+MAX_SAFE_INTEGER							retorna o número inteiro seguro máximo
+VALOR MÁXIMO								retorna o maior valor possível
+MIN_SAFE_INTEGER							retorna o número inteiro seguro mínimo
+MIN_VALUE									retorna o menor valor possível
+NaN											representa o valor 'Não é um número'
+NEGATIVE_INFINITY							representa infinito negativo
+POSITIVE_INFINITY							representa infinito positivo
+protótipo									permite a adição de propriedades aos objetos Number
+
+*/
+
+
+
+// Vamos ver 2 exemplos
+
+const a = Number.MAX_VALUE;
+console.log(a); 
+
+
+const b = Number.MAX_SAFE_INTEGER;
+console.log(b);
+
+
---
+++ Aula-43.js
@@ -0,0 +1,16 @@
+
+// Função Number JS
+
+
+ const a = '23';
+ const b = true;
+
+const res1 = Number(a);
+const resul2 = Number(b);
+
+
+console.log(res1);
+console.log(resul2);
+
+
+
