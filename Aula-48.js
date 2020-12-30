714a434ef83a5da1a33645df955ad9705b5c76dd
Leonardo Lourenco <lnddru@gmail.com>
2020-12-30 19:17:22 -0300

    Add files via upload

---
+++ Aula-48.js
@@ -0,0 +1,65 @@
+
+
+// Array JavaScript
+
+
+
+const matrizTESTE = ['Leo', 'Coffee', 'Tag'];
+
+
+// 1 - Array Literal
+
+const matrizTESTE = ['Leo', 'Coffee', 'Tag'];
+
+
+// 2 - New
+
+const matrizDOIS = new Array("Cooffee", "Tag");
+
+
+
+// array vazio
+const lista = [ ];
+
+// array  com vários valores
+const lista2 = [ 2 , 4, 5, 6];
+
+// array com várias strings
+const lista3 = [ 'Leo', 'Coffee', 'tag'];
+
+// arrays com diferentes tipos de dados
+
+const lista4 = ['Leo', 'Coffee' , 1 , true];
+
+
+
+// aramazenando funções e outros objetos dentro da Matriz
+
+const listaVARIADA = [
+
+	{'Leo' : 'Coffee'},
+	[1, 2, 3],
+	function hello(){ cosole.log('Hello')}
+
+
+
+];
+
+
+
+// acessando elementos de uma matriz
+
+const lista = ['c', 'o' , 'f', 'f', 'e', 'e']; // 6 elememtos
+
+console.log(lista[0]); // c
+
+
+console.log(lista[2]); // f
+
+
+
+
+
+//  c  o  f  f  e   e 
+//  0  1  2  3  4   5
+
